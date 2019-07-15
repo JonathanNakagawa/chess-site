d1f0f300571023c7a73eb768d2a7a3dfda4a2b7e
Jonathan Nakagawa <jnakagawa17@gmail.com>
2019-07-14 16:51:41 -1000

    Modified to add routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Login from './components/login/login.js'
+import Login from './components/login/login.js';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
